fd73db36b9a28e15fc4c1c974dce2f7b24587bf6
Maxim Murzanev <1maxmeur1gmail.com>
2021-06-05 16:28:52 +0400

    Realization of LinkedList

--- main.js
+++ main.js
@@ -1,1 +1,112 @@
 "use strict"
+
+//Constant Time: O(1)
+//Logarithmic Time: O(log(n))
+// Linear Time: O(1)
+// Linearithmic Time: O(nlog(n))
+// Quadric Time: O(n^2)
+// Cubic Time: O(n^3)
+// Exponential Time: O(b^n), b > 1
+// Factorial Time: O(n!)
+
+/*-------------------------------------REALIZATION OF DOUBLY LINKEDLIST-----------------------------------*/
+
+class Node { // Declare Node
+	constructor(value) {
+		this.value = value
+		this.next = null
+		this.previous = null
+	}
+}
+
+class LinkedList {
+	constructor() {
+		this.head = null
+		this.tail = null
+		this.length = 0
+	}
+
+	size() {
+		return this.length
+	}
+
+	print() { //Print every node in array
+
+		let result = []
+		let current = this.head
+
+		while(current) {
+			result.push(current.value)
+			current = current.next
+		}
+
+		return result
+	}
+
+	search(value) { //Search element in the linkedList by its value
+		let current = this.head
+
+		while(current) {
+			if(current.value == value) {
+				return current
+			}
+			current = current.next
+		}
+
+		return null
+	}
+
+	addToHead(value) {
+		let node = new Node(value)
+
+		if(this.head) { //Connect node and the head. Declare node as this.head
+			node.next = this.head
+			this.head.previous = node
+			this.head = node
+		} else {  
+			this.head = node
+			this.tail = node
+		}
+		this.length++
+	}
+
+	addToTail(value) {
+		let node = new Node(value)
+
+		if(this.tail) { //Connect node and the tail. Declare node as this.tail and update pointers
+			node.previous = this.tail
+			this.tail.next = node
+			this.tail = node
+			this.tail.next = null
+		} else {
+			this.head = node
+			this.tail = node
+		}
+		this.length++
+	}
+
+	removeFromHead() {
+		if(!this.head) {
+			return null
+		} else { //Declare next node as the this.head and update pointers
+			this.head = this.head.next
+			this.head.previous = null
+		}
+		this.length--
+	}
+
+	removeFromTail() {
+		if(!this.tail) {
+			return null
+		} else {//Declare previous node as the this.tail and update pointers
+			this.tail = this.tail.previous
+			this.tail.next = null
+		}
+		this.length--
+	}
+}
+
+
+let ll = new LinkedList() //Declare LInkedList
+
+
